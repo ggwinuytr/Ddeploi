8342190b5b1e66db337f1b0a8e8a05b48b37a869
Plovchikdeval <plov99502@gmail.com>
2024-12-16 15:48:34 +0500

    Update main.py

--- hikka/main.py
+++ hikka/main.py
@@ -98,6 +98,7 @@ IS_AWS = "AWS_OS" in os.environ
 IS_SERV00 = "serv00" in socket.gethostname()
 IS_TOTHOST = "tothost" in socket.gethostname()
 IS_AEZA = "aeza" in socket.gethostname()
+IS_DJHOST = "djhost" in socket.gethostname()
 IS_WSL = False
 with contextlib.suppress(Exception):
     from platform import uname
